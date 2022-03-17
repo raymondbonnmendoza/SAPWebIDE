ab1ca9788a6aeae88e7d35b64dcf271c12ade334
raymondbonnmendoza <mendozarb@gmail.com>
2022-03-17 17:22:11 +0800

    Initial Commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("SmartTemplateExercise.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("SmartTemplateExercise.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
